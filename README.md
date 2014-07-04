3c56b524b92ae07fe77f889d6f04f309de82d034
Paul Kim <paulhyunkim@gmail.com>
2014-07-03 19:10:35 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+## WDIGram
+
+Built as my second project at WDI
+
+Simple rails photoblog app where students of WDI 5/6 can upload photos throughout the course. Mobile responsive.
+
+Built using: 
+* Ruby on Rails
+* HTML/CSS
+* Bootstrap
+* MongoDB
+* Paperclip gem
+* AWS cloud storage
+* Heroku deployed
+
+
+http://wdigram.herokuapp.com
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,34 +38,6 @@ class User
   validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 
-  def password
-    @password
-  end
+ 
 
-  def password=(new_password)
-    @password = new_password
-    self.password_digest = BCrypt::Password.create(new_password)
-  end
-
-  def authenticate(test_password)
-    if BCrypt::Password.new(self.password_digest) == test_password
-      self
-    else
-      false
-    end
-  end
-
-  def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def User.digest(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-
-  private
-
-    def create_remember_token
-      self.remember_token = User.digest(User.new_remember_token)
-    end
 end
